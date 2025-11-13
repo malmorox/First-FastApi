93b85eb295b2647b0c34da2bc899db419d07b7b2
malmorox <marcos.almorox@gmail.com>
2025-11-13 13:02:49 +0100

    cambios varios

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+FAKE_USER = {
+    "username": "test",
+    "password": "1234"
+}
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "API levantada"}
+
+
+@app.post("/login")
+def login(data: LoginRequest):
+    if data.username == FAKE_USER["username"] and data.password == FAKE_USER["password"]:
+        
+        return {
+            "success": True,
+            "message": "Login correcto"
+        }
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciales incorrectas"
+        )
